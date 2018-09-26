be455118826b81deb64b490e76d2f057d1309e1c
Seo-young Silvia Kim <sskim@caltech.edu>
2018-09-25 22:05:33 -0700

    Document FIPS dataset

---
+++ R/fips.R
@@ -0,0 +1,14 @@
+#' State-level Geographic Indicators
+#'
+#' A dataset containing the geographic indicators for U.S. states,
+#' including the District of Columbia.
+#'
+#' @format A data frame with 51 rows and 5 variables:
+#' \describe{
+#'   \item{stname}{State names, in lowercase}
+#'   \item{stabb}{State abbreviations}
+#'   \item{stfips}{FIPS code}
+#'   \item{south_all}{Southern states}
+#'   \item{south_conf}{Southern states (11 confederate)}
+#' }
+"fips"
