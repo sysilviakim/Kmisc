66969b176719e5f06f10ae580ab88b6c5067fd61
Seo-young Silvia Kim <sskim@caltech.edu>
2019-02-16 19:53:53 -0800

    Add dir_dendrogram

---
+++ R/dir_dendrogram.R
@@ -0,0 +1,23 @@
+#' Directory Tree Dendrogram
+#'
+#' This function takes a vector of directory structures and turns it into a
+#' dendrogram. The code is a variation of Tyler Rinker's StackOverflow post
+#' here: https://stackoverflow.com/questions/36094183/how-to-build-a-dendrogram-from-a-directory-tree
+#' Note that you can print everything by such as following:
+#' `print(dir_dendrogram(list.dirs()), limit = 300)``
+
+#' @importFrom data.tree as.Node
+#' @importFrom stats na.omit
+#' @import dplyr
+#' @param path List of directories to be converted to a dendrogram
+
+#' @export
+
+dir_dendrogram <- function(path) {
+  x <- lapply(strsplit(path, "/"), function(z) as.data.frame(t(z)))
+  x <- suppressWarnings(dplyr::rbind_all(x))
+  x$pathString <- apply(
+    x, 1, function(x) paste(trimws(na.omit(x)), collapse = "/")
+  )
+  return(data.tree::as.Node(x))
+}
