cb3ba4c958fe5c20f1e8bb66b24916bfbc284a62
Seo-young Silvia Kim <sskim@caltech.edu>
2019-05-14 21:19:58 -0700

    Clean png_default package imports

--- R/png_default.R
+++ R/png_default.R
@@ -7,6 +7,8 @@
 #' @import extrafont
 #' @import grDevices
 #' @import ggplot2
+#' @import ggthemes
+#'
 #' @param p The input ggplot2 object
 #' @param font Font for the graphics. Default is NULL.
 #' @param legend.position Legend position passed to ggplot2. Default is bottom.
@@ -22,9 +24,9 @@
 png_default <- function(p, font = NULL,
                         legend.position = "bottom",
                         legend.direction = "horizontal") {
-  extrafont::loadfonts()
+  loadfonts()
   if (Sys.info()["sysname"] == "Windows") {
-    extrafont::loadfonts(device = "win")
+    loadfonts(device = "win")
   }
   if (is.null(font)) {
     font <- "sans"
@@ -38,17 +40,17 @@ png_default <- function(p, font = NULL,
     font = "font"
   }
   p <- p +
-    ggthemes::theme_economist() +
-    ggthemes::scale_colour_economist() +
-    ggplot2::theme(
-      plot.title = ggplot2::element_text(family = font),
-      text = ggplot2::element_text(family = font),
-      axis.text.x = ggplot2::element_text(family = font),
-      axis.text.y = ggplot2::element_text(family = font),
-      legend.text = ggplot2::element_text(family = font),
+    theme_economist() +
+    scale_colour_economist() +
+    theme(
+      plot.title = element_text(family = font),
+      text = element_text(family = font),
+      axis.text.x = element_text(family = font),
+      axis.text.y = element_text(family = font),
+      legend.text = element_text(family = font),
       legend.position = legend.position,
       legend.direction = legend.direction,
-      legend.title = ggplot2::element_blank()
+      legend.title = element_blank()
     )
   return(p)
 }
