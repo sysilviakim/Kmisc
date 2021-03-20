38344ade4489802efecf3bb7a2a674ff08bc80e2
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-03-20 17:53:28 -0400

    incorporate useNA to prop

--- R/prop.R
+++ R/prop.R
@@ -11,21 +11,30 @@
 #' Defaults to NULL.
 #' @param print Whether to only print the output, so that no quotes are seen
 #' after applying \code{formatC}.
+#' @param useNA useNA which is an argument for \code{table}.
+#' Defaults to "ifany."
 #'
 #' @export
 
-prop <- function(df, vars, digit = 1, sort = NULL, head = NULL, print = TRUE) {
+prop <- function(df, vars, digit = 1, sort = NULL, head = NULL, print = TRUE,
+                 useNA = "ifany") {
   if (length(vars) > 2) {
     stop("Too many variables.")
   }
   if (length(vars) < 1) {
     stop("Invalid vars argument.")
   }
+  if (!(useNA %in% c("no", "ifany", "always"))) {
+    stop("Invalid useNA argument.")
+  }
+
   if (length(vars) == 1) {
-    temp <- prop.table(table(df[[vars]], dnn = vars)) * 100
+    temp <- prop.table(table(df[[vars]], dnn = vars, useNA = useNA)) * 100
   }
   if (length(vars) == 2) {
-    temp <- prop.table(table(df[[vars[1]]], df[[vars[2]]], dnn = vars)) * 100
+    temp <- prop.table(
+      table(df[[vars[1]]], df[[vars[2]]], dnn = vars, useNA = useNA)
+    ) * 100
   }
 
   if (!is.null(sort)) {
