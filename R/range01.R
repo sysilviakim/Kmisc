68b37d2ca625d3dedd4bc26dba2eea90cf15ce7d
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2025-08-26 10:51:02 +0900

    na.rm param for range01

--- R/range01.R
+++ R/range01.R
@@ -4,13 +4,18 @@
 #' and rescales it to be between 0 and 1.
 #'
 #' @param x A numeric vector to be rescaled.
+#' @param na.rm A logical value indicating whether to remove NA values before computation. Default is FALSE.
+#'
 #' @return A rescaled numeric vector.
 #'
 #' @export
 
-range01 <- function(x) {
-  if (class(x) != "numeric" & class(x) != "integer") {
+range01 <- function(x, na.rm = FALSE) {
+  if (is.numeric(x) == FALSE & is.integer(x) == FALSE) {
     stop("x is neither integer or numeric class.")
   }
-  return((x - min(x)) / (max(x) - min(x)))
+  return(
+    (x - min(x, na.rm = na.rm)) /
+      (max(x, na.rm = na.rm) - min(x, na.rm = na.rm))
+  )
 }
